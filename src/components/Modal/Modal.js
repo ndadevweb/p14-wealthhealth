157b36edacc651608fbb22858d420d87b3a60024
Nicolas D <oc_nicolasd>
2023-07-19 14:58:42 +0200

    feature: Add focus on the modal's button #23
    Resolved #23

--- src/components/Modal/Modal.js
+++ src/components/Modal/Modal.js
@@ -1,8 +1,11 @@
 import { themeOverlay, themeModal, themeContent, themeButtonClose } from './utils'
 import classes from './Modal.module.css'
+import { useEffect, useRef } from 'react'
 
 /**
  * Modal component
+ *
+ * When displayed the button has focus
  *
  * @param {Object}   props
  * @param {Object}   props.children
@@ -15,18 +18,30 @@ import classes from './Modal.module.css'
  */
 export default function Modal({ children, open, callbackClose, labelButton, themes }) {
 
+  const refModal = useRef(null)
+
+  useEffect(() => {
+    if(open === true) {
+      refModal.current.querySelector('[type="button"]').focus()
+    }
+  }, [open])
+
   if(open === false) {
     return null
   }
 
   return (
     <div className={ themeOverlay(themes, classes) } onClick={ () => callbackClose() }>
-      <div role="dialog" className={ themeModal(themes, classes) } onClick={ (event) => event.stopPropagation() }>
+      <div role="dialog" className={ themeModal(themes, classes) } onClick={ (event) => event.stopPropagation() } ref={ refModal }>
         <div className={ themeContent(themes, classes) }>
           { children }
         </div>
 
-        <button type="button" className={ themeButtonClose(themes, classes) } onClick={ () => callbackClose() }>
+        <button
+          type="button"
+          className={ themeButtonClose(themes, classes) }
+          onClick={ () => callbackClose() }
+        >
           { labelButton || '✖' }
         </button>
       </div>
